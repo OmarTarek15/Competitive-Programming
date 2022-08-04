fbb48c6a5282384a1fb99084eec068198b862d24
OmarTarek15 <81865160+OmarTarek15@users.noreply.github.com>
2022-08-04 15:13:20 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1186A | Vus the Cossack and a Contest/149547058.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+ 
+ 
+void solve(){
+   int n, m, k; cin>>n>>m>>k;
+   if(m >= n && k >= n)
+       cout<<"Yes";
+   else
+       cout<<"No";
+}
+ 
+int main(){
+    fast
+    //int t; cin>>t;
+    //while(t--){
+        solve();
+    //}
+}
