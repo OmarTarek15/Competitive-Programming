64f027c822fdd58250b9cdf4ae62c949097158fc
OmarTarek15 <81865160+OmarTarek15@users.noreply.github.com>
2022-08-04 15:17:52 +0200

    Committing files

---
+++ CodeForces/GNU C++17/766B | Mahmoud and a Triangle/121726731.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+ 
+int main(){
+    int n; cin>>n;
+    int a[n];
+    for(int i = 0; i < n; ++i)
+        cin>>a[i];
+    sort(a, a+n);
+    for(int i = 0; i < n-2; ++i)
+        if(a[i] + a[i+1] > a[i+2]){
+            cout<<"YES";
+            return 0;
+        }
+    cout<<"NO";
+}
