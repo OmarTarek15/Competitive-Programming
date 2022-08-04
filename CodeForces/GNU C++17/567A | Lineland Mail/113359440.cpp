ae0a688501f2e5ae11fc756010425f947b425c89
OmarTarek15 <81865160+OmarTarek15@users.noreply.github.com>
2022-08-04 15:20:15 +0200

    Committing files

---
+++ CodeForces/GNU C++17/567A | Lineland Mail/113359440.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+ 
+int main(){
+    int n; cin>>n;
+    int arr[n];
+    for(int i = 0; i < n; ++i) cin>>arr[i];
+    long long mini, maxi;
+    for(int i = 0; i < n; ++i){
+        if(i == 0){
+            mini = abs(arr[i] - arr[i+1]);
+            maxi = abs(arr[i] - arr[n-1]);
+        }else if(i == n-1){
+            mini = abs(arr[i] - arr[i-1]);
+            maxi = abs(arr[i] - arr[0]);
+        }else{
+            mini = min(abs(arr[i] - arr[i-1]), abs(arr[i] - arr[i+1]));
+            maxi = max(abs(arr[i] - arr[0]), abs(arr[i] - arr[n-1]));
+        }
+        cout<<mini<<" "<<maxi<<endl;
+    }
+}
