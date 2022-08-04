cf7aa5cfdec5557286614d869cc2b515e7701a2c
OmarTarek15 <81865160+OmarTarek15@users.noreply.github.com>
2022-08-04 15:15:00 +0200

    Committing files

---
+++ CodeForces/GNU C++17/479A | Expression/145583080.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+ 
+int main(){
+    int a, b, c, ans;
+    while( cin>>a>>b>>c ){
+        ans = a + b + c;
+        ans = max(ans, a+b*c);
+        ans = max(ans, a*(b+c));
+        ans = max(ans, a*b*c);
+        ans = max(ans, (a+b)*c);
+        cout<<ans;
+    }
+}
