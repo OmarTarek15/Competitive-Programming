57e8c2f52412378bcaf0585af78d9b88a11eb845
OmarTarek15 <81865160+OmarTarek15@users.noreply.github.com>
2022-08-04 15:21:26 +0200

    Committing files

---
+++ CodeForces/GNU C++17/236A | Boy or Girl/110981016.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+ 
+int main() {
+   string s; cin>>s;
+   int cnt = 0;
+   sort(s.begin(), s.end());
+   for(int i = 0; i < s.size(); ++i){
+        if(s[i] != s[i+1]) cnt++;
+        else continue;
+   }
+   if(cnt % 2 == 0) cout<<"CHAT WITH HER!";
+   else cout<<"IGNORE HIM!";
+}
