c6c6d9e366d79a9dfa758469de9fb5d11af187de
OmarTarek15 <81865160+OmarTarek15@users.noreply.github.com>
2022-08-04 15:14:43 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1335A | Candies and Two Sisters/145869054.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+ 
+ 
+int main(){
+    int t; cin>>t;
+    ll n;
+    while(t--){
+        cin>>n;
+        cout<<(n-1)/2<<"\n";
+    }
+}
