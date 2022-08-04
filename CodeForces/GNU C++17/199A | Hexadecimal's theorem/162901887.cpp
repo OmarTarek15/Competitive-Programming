37241230277f52067443d05dc49cf8fe87cda0c5
OmarTarek15 <81865160+OmarTarek15@users.noreply.github.com>
2022-08-04 15:12:35 +0200

    Committing files

---
+++ CodeForces/GNU C++17/199A | Hexadecimal's theorem/162901887.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define IO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long
+ 
+using namespace std;
+ 
+void solve(){
+    int n; cin>> n;
+    cout<<"0 0 "<<n;
+}
+ 
+int main() {
+    IO
+    solve();
+}
