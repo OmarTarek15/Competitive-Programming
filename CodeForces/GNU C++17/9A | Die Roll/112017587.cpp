4058aacf430e8f0995250f06203218dec356dc7b
OmarTarek15 <81865160+OmarTarek15@users.noreply.github.com>
2022-08-04 15:20:52 +0200

    Committing files

---
+++ CodeForces/GNU C++17/9A | Die Roll/112017587.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+ 
+ 
+using namespace std;
+ 
+int main(){
+    int a, b; cin>>a>>b;
+    string s[7] = {"", "1/1", "5/6", "2/3", "1/2", "1/3", "1/6"};
+    int i = max(a, b);
+    cout<<s[i];
+}
